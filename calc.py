6cc7c79778000a0755d7804c27ee3c4c09bfe28d
Osvin Alaphat <140211517+osvinalaphat@users.noreply.github.com>
2025-04-13 14:25:39 -0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
 num1 = input("Enter first number: ")
-num2 = input("Enter second number:")
+num2 = input("Enter second number: ")
+op = input("enter an operation: ")
 
-print(num1+num2)
+if op == '+':
+  print(num1+num2)
